c5d81aa1acad9ec0fc99a3c1abd3726447723680
iyeahh <yboravender@gmail.com>
2024-09-19 13:53:05 +0900

    Initial Commit

---
+++ NoDamHaJang/NoDamHaJangApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoDamHaJangApp.swift
+//  NoDamHaJang
+//
+//  Created by Bora Yang on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct NoDamHaJangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
